261f75d6c0b17e09a8d7009999f482983521e577
LittleBird <2250125690@qq.com>
2021-05-22 14:34:53 +0800

    update js/index.js.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-document.oncontextmenu = function() {
-  return false;
+window.oncontextmenu = function (e) {
+    // 禁用右键菜单
+    e.preventDefault();
+}
+window.onselectstart = function(e) {
+    // 禁用鼠标拖动选中
+    e.preventDefault();
 }
